b6c612e9d3783176443aff3cf2e99470cde11a65
TIHELP <t.i@helpmultas.com>
2026-05-19 09:51:37 -0300

    FIX: PixelMETA

--- src/main.tsx
+++ src/main.tsx
@@ -9,17 +9,17 @@ declare global {
   }
 }
 
-// Inicializa o Meta Pixel
 if (!window.fbq) {
+  window.fbq = function() {
+    (window.fbq.q = window.fbq.q || []).push(arguments);
+  };
+  window.fbq.q = window.fbq.q || [];
+
   const script = document.createElement('script');
   script.async = true;
   script.src = 'https://connect.facebook.net/en_US/fbevents.js';
   document.head.appendChild(script);
 
-  window.fbq = function() {
-    (window.fbq.q = window.fbq.q || []).push(arguments);
-  };
-  window.fbq.q = window.fbq.q || [];
   window.fbq('init', '968941662364020');
   window.fbq('track', 'PageView');
 }
