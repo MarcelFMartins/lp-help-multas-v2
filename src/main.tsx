9e6991921a6b131d18f0edf68506bb0467137fa4
TIHELP <t.i@helpmultas.com>
2026-05-19 09:48:06 -0300

    FIX: Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// 🔥 META PIXEL - Carrega PageView na página index
 declare global {
   interface Window {
     fbq: any;
